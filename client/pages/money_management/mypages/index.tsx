73e0cece1c43a01aea7015c79071953551bee1ab
hideto <ma_kux@icloud.com>
2024-09-15 18:22:46 +0900

    修正

--- client/pages/money_management/mypages/index.tsx
+++ client/pages/money_management/mypages/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { mypage_classes } from '@/common/linkpages'
-import SideBar from '../SideBar'
 import { BoxLayoutPadding, Breadcrumbs } from '@/common/component';
 
 
 const Mypage: React.FC = () => {
   return (
     <BoxLayoutPadding>
-        <Breadcrumbs
-          marginBottom="12px"/> {/* パンくずを表示する */}
-        <h1>マイページ</h1>
-      <SideBar classes={mypage_classes}/>
+      <Breadcrumbs
+        marginBottom="12px"/> {/* パンくずを表示する */}
+      <h1>マイページ</h1>
     </BoxLayoutPadding>
   );
 };
