d1493f3e97ee2af83301a96da369c2e89d932c3d
hideto <ma_kux@icloud.com>
2024-11-24 14:04:40 +0900

    初期状態に戻す

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -4,7 +4,6 @@ module.exports = {
     moduleNameMapper: {
         '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
         '^@/(.*)$': '<rootDir>/src/$1',
-        '^next/router$': '<rootDir>/__mocks__/nextRouterMock.js', // next/routerのモックファイル
     },
     transform: {
         '^.+\\.(ts|tsx)$': 'ts-jest',
@@ -17,5 +16,4 @@ module.exports = {
             },
         },
     },
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
