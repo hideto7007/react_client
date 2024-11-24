39d0c85dbd87809db16a07b984026c83858fc760
hideto <ma_kux@icloud.com>
2024-11-24 23:35:00 +0900

    jestの設定についてコメント追加

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -6,7 +6,7 @@ module.exports = {
         '^@/(.*)$': '<rootDir>/$1',
     },
     transform: {
-        '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: { jsx: 'react-jsx' } }],
+        '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: { jsx: 'react-jsx' } }], // DOM要素を取得するための設定(react-jsx)
         '^.+\\.(js|jsx)$': ['babel-jest', { presets: ['next/babel'] }],
     },
     setupFilesAfterEnv: ['<rootDir>/setup-jest.ts']
