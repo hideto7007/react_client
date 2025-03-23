f56ad04ea821a5dd92f88273d37a066ee694a503
hideto <ma_kux@icloud.com>
2025-03-23 17:17:19 +0900

    リファクタ

--- src/constants/entity.ts
+++ src/constants/entity.ts
@@ -1,4 +1,4 @@
-import { BoxProps, CircularProgressProps } from '@mui/material'
+import { BoxProps, CircularProgressProps } from "@mui/material";
 import {
   Control,
   ControllerFieldState,
@@ -12,248 +12,271 @@ import {
   UseFormRegister,
   UseFormSetError,
   UseFormSetValue,
-} from 'react-hook-form'
+} from "react-hook-form";
 
 // インターフェース定義
 interface AnnualIncomeManagementKey {
-  payment_date: string
-  age: number
-  industry: string
-  total_amount: number
-  deduction_amount: number
-  take_home_amount: number
-  edit: string
+  payment_date: string;
+  age: number;
+  industry: string;
+  total_amount: number;
+  deduction_amount: number;
+  take_home_amount: number;
+  edit: string;
 }
 
 // インターフェース定義
 interface AnnualIncomeManagementKeyNotEdit {
-  payment_date: string
-  age: number
-  industry: string
-  total_amount: number
-  deduction_amount: number
-  take_home_amount: number
+  payment_date: string;
+  age: number;
+  industry: string;
+  total_amount: number;
+  deduction_amount: number;
+  take_home_amount: number;
 }
 
 // インターフェース定義
 interface AnnualIncomeManagement {
-  payment_date: string
-  age: number
-  industry: string
-  total_amount: number
-  deduction_amount: number
-  take_home_amount: number
+  payment_date: string;
+  age: number;
+  industry: string;
+  total_amount: number;
+  deduction_amount: number;
+  take_home_amount: number;
 }
 
 interface AnnualIncomeManagementData extends AnnualIncomeManagement {
-  id: number
-  income_forecast_id: string
-  update_user: string
-  classification: string
-  user_id: number
+  id: number;
+  income_forecast_id: string;
+  update_user: string;
+  classification: string;
+  user_id: number;
 }
 
-type AnnualIncomeManagementCreateData = AnnualIncomeManagementData
+type AnnualIncomeManagementCreateData = AnnualIncomeManagementData;
 
-type AnnualIncomeManagementUpdateData = AnnualIncomeManagementData
+type AnnualIncomeManagementUpdateData = AnnualIncomeManagementData;
 
-type AnnualIncomeManagementDeleteData = AnnualIncomeManagementData
+type AnnualIncomeManagementDeleteData = AnnualIncomeManagementData;
 
-type Order = 'asc' | 'desc'
+type Order = "asc" | "desc";
 
 interface Validate {
-  payment_date: string | boolean | null
-  age: string | boolean | null
-  industry: string | boolean | null
-  total_amount: string | boolean | null
-  deduction_amount: string | boolean | null
-  take_home_amount: string | boolean | null
-  classification: string | boolean | null
+  payment_date: string | boolean | null;
+  age: string | boolean | null;
+  industry: string | boolean | null;
+  total_amount: string | boolean | null;
+  deduction_amount: string | boolean | null;
+  take_home_amount: string | boolean | null;
+  classification: string | boolean | null;
+}
+
+interface RequestDepositCalculation {
+  money_received?: number;
+  bouns?: number;
+  fixed_cost?: number;
+  loan?: number;
+  private?: number;
+  insurance?: number;
 }
 
 interface Column {
-  id: keyof AnnualIncomeManagementKey // AnnualIncomeManagementData のプロパティ名のいずれか
-  label: string
-  minWidth?: number
-  disablePadding: boolean
-  align?: 'right' | 'left' | 'center'
-  format?: (value: number) => string
-  required: boolean
+  id: keyof AnnualIncomeManagementKey; // AnnualIncomeManagementData のプロパティ名のいずれか
+  label: string;
+  minWidth?: number;
+  disablePadding: boolean;
+  align?: "right" | "left" | "center";
+  format?: (value: number) => string;
+  required: boolean;
 }
 
 interface ColumnNotEdit {
-  id: keyof AnnualIncomeManagementKeyNotEdit // AnnualIncomeManagementData のプロパティ名のいずれか
-  label: string
-  minWidth?: number
-  disablePadding: boolean
-  align?: 'right' | 'left' | 'center'
-  format?: (value: number) => string
-  required: boolean
+  id: keyof AnnualIncomeManagementKeyNotEdit; // AnnualIncomeManagementData のプロパティ名のいずれか
+  label: string;
+  minWidth?: number;
+  disablePadding: boolean;
+  align?: "right" | "left" | "center";
+  format?: (value: number) => string;
+  required: boolean;
 }
 
 interface EnhancedTableProps {
-  numSelected: number
+  numSelected: number;
   onRequestSort: (
     event: React.MouseEvent<unknown>,
     property: keyof AnnualIncomeManagementKeyNotEdit
-  ) => void
-  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void
-  order: Order
-  orderBy: string
-  rowCount: number
+  ) => void;
+  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
 }
 
 interface EnhancedTableToolbarProps {
-  numSelected: number
-  selected: readonly number[] // 選択された行のIDを含む配列
-  data: AnnualIncomeManagementDeleteData[] // 選択された行のデータ
+  numSelected: number;
+  selected: readonly number[]; // 選択された行のIDを含む配列
+  data: AnnualIncomeManagementDeleteData[]; // 選択された行のデータ
   onDelete: (
     data: AnnualIncomeManagementDeleteData[],
     selected: readonly number[]
-  ) => void // 削除時に呼ばれる関数
-  checkboxLabel: string
-  checked: boolean
-  onCheckBox: (event: React.ChangeEvent<HTMLInputElement>) => void
+  ) => void; // 削除時に呼ばれる関数
+  checkboxLabel: string;
+  checked: boolean;
+  onCheckBox: (event: React.ChangeEvent<HTMLInputElement>) => void;
   // onCheckBox: (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => void
 }
 
 interface editDialogProps {
-  editDialogLabel: string
+  editDialogLabel: string;
   // checked: boolean
-  dialogOpen: boolean
-  row: AnnualIncomeManagementDeleteData | null
-  handleClose: () => void
+  dialogOpen: boolean;
+  row: AnnualIncomeManagementDeleteData | null;
+  handleClose: () => void;
 }
 
 interface CsvFileSetting {
-  file: File | null
+  file: File | null;
 }
 
 interface FileSelectProps {
-  setValue: UseFormSetValue<CsvFileSetting>
-  register: UseFormRegister<CsvFileSetting>
-  control: Control<CsvFileSetting, unknown>
-  errors: FieldErrors<CsvFileSetting>
-  setError: UseFormSetError<CsvFileSetting>
-  clearErrors: UseFormClearErrors<CsvFileSetting>
+  setValue: UseFormSetValue<CsvFileSetting>;
+  register: UseFormRegister<CsvFileSetting>;
+  control: Control<CsvFileSetting, unknown>;
+  errors: FieldErrors<CsvFileSetting>;
+  setError: UseFormSetError<CsvFileSetting>;
+  clearErrors: UseFormClearErrors<CsvFileSetting>;
 }
 
 interface AuthFormProps {
-  user_email: string
-  user_password: string
-  confirm_password?: string // Sign Upの場合のみ必要
-  user_name?: string // Sign Upの場合のみ必要
+  user_email: string;
+  user_password: string;
+  confirm_password?: string; // Sign Upの場合のみ必要
+  user_name?: string; // Sign Upの場合のみ必要
 }
 
 interface NewPasswordUpdateProps {
-  token_id: string
-  new_user_password: string
-  confirm_password: string
+  token_id: string;
+  new_user_password: string;
+  confirm_password: string;
 }
 
 interface EmailCheckProps {
-  user_email: string
+  user_email: string;
 }
 
 interface PasswordResetProps {
-  current_password: string
-  new_user_password: string
-  confirm_password: string
+  current_password: string;
+  new_user_password: string;
+  confirm_password: string;
 }
 
 interface SingUpProps {
-  redis_key: string
-  auth_email_code: string
+  redis_key: string;
+  auth_email_code: string;
 }
 
 interface EntryAuthEmailProps {
-  redis_key: string
-  user_email: string
-  user_name: string
+  redis_key: string;
+  user_email: string;
+  user_name: string;
 }
 
-type RequestSigninProps = AuthFormProps
+type RequestSigninProps = AuthFormProps;
 
-type RequestTmpSignUpResProps = AuthFormProps
+type RequestTmpSignUpResProps = AuthFormProps;
 
-type RequestNewPasswordUpdateProps = NewPasswordUpdateProps
+type RequestNewPasswordUpdateProps = NewPasswordUpdateProps;
 
 interface RequestSignUpProps {
-  redis_key: string
-  auth_email_code: string
+  redis_key: string;
+  auth_email_code: string;
 }
 
 interface PasswordResetReqProps {
-  data: PasswordResetProps[]
+  data: PasswordResetProps[];
 }
 
 interface RequestDataProps<T> {
-  data: T
+  data: T;
 }
 
 interface RequestExternalAuth {
-  user_email: string
-  user_name: string
+  user_email: string;
+  user_name: string;
 }
 
 interface PasswordFormProps {
-  name: keyof AuthFormProps
-  label: string
-  control: Control<AuthFormProps> // Tに依存するControl型
+  name: keyof AuthFormProps;
+  label: string;
+  control: Control<AuthFormProps>; // Tに依存するControl型
   rules?:
     | Omit<
         RegisterOptions<AuthFormProps>,
-        'disabled' | 'valueAsNumber' | 'valueAsDate' | 'setValueAs'
+        "disabled" | "valueAsNumber" | "valueAsDate" | "setValueAs"
       >
-    | undefined
-  fieldState?: ControllerFieldState
-  fullWidth?: boolean
+    | undefined;
+  fieldState?: ControllerFieldState;
+  fullWidth?: boolean;
 }
 
 interface PasswordUpdateFormProps {
-  name: keyof NewPasswordUpdateProps
-  label: string
-  control: Control<NewPasswordUpdateProps> // Tに依存するControl型
+  name: keyof NewPasswordUpdateProps;
+  label: string;
+  control: Control<NewPasswordUpdateProps>; // Tに依存するControl型
   rules?:
     | Omit<
         RegisterOptions<NewPasswordUpdateProps>,
-        'disabled' | 'valueAsNumber' | 'valueAsDate' | 'setValueAs'
+        "disabled" | "valueAsNumber" | "valueAsDate" | "setValueAs"
       >
-    | undefined
-  fieldState?: ControllerFieldState
-  fullWidth?: boolean
+    | undefined;
+  fieldState?: ControllerFieldState;
+  fullWidth?: boolean;
 }
 
 interface TextFormProps<T extends FieldValues> {
-  name: Path<T>
-  label: string
-  control: Control<T> // Tに依存するControl型
+  name: Path<T>;
+  label: string;
+  control: Control<T>; // Tに依存するControl型
   rules?:
     | Omit<
         RegisterOptions<T>,
-        'disabled' | 'valueAsNumber' | 'valueAsDate' | 'setValueAs'
+        "disabled" | "valueAsNumber" | "valueAsDate" | "setValueAs"
       >
-    | undefined
-  fieldState?: ControllerFieldState
-  fullWidth?: boolean
+    | undefined;
+  fieldState?: ControllerFieldState;
+  fullWidth?: boolean;
+}
+
+interface DepositCalculationFormProps {
+  name: RequestDepositCalculation;
+  label: string;
+  control: Control<RequestDepositCalculation>;
+  rules?:
+    | Omit<
+        RegisterOptions<RequestDepositCalculation>,
+        "disabled" | "valueAsNumber" | "valueAsDate" | "setValueAs"
+      >
+    | undefined;
+  fieldState?: ControllerFieldState;
+  fullWidth?: boolean;
 }
 
 interface SideBarProps {
-  name: string
-  link: string
+  name: string;
+  link: string;
 }
 
 interface ClassesProps {
-  anchor: 'bottom' | 'left' | 'right' | 'top'
-  classes: SideBarProps[]
-  open: boolean // サイドバーの開閉状態
+  anchor: "bottom" | "left" | "right" | "top";
+  classes: SideBarProps[];
+  open: boolean; // サイドバーの開閉状態
   toggleDrawer: (
     open: boolean
-  ) => (event: React.KeyboardEvent | React.MouseEvent) => void
+  ) => (event: React.KeyboardEvent | React.MouseEvent) => void;
 }
 
 interface ToolbarProps {
-  disabled: boolean
+  disabled: boolean;
 }
 
 // interface BoxProps {
@@ -272,94 +295,99 @@ interface ToolbarProps {
 // }
 
 interface BreadcrumbsProps {
-  marginBottom: string
+  marginBottom: string;
 }
 
 interface PathMapProps {
-  about: string
-  posts: string
-  signup: string
-  signin: string
-  table: string
-  table1: string
-  csvimport: string
+  about: string;
+  posts: string;
+  signup: string;
+  signin: string;
+  table: string;
+  table1: string;
+  csvimport: string;
 }
 
-type CsvImportMainProps = object
+type CsvImportMainProps = object;
 
 interface TWToastProps {
-  open: boolean
-  handleClose: () => void
-  vertical: 'top' | 'bottom'
-  horizontal: 'center' | 'right' | 'left'
-  severity: 'success' | 'info' | 'warning' | 'error'
-  message: string
+  open: boolean;
+  handleClose: () => void;
+  vertical: "top" | "bottom";
+  horizontal: "center" | "right" | "left";
+  severity: "success" | "info" | "warning" | "error";
+  message: string;
 }
 
 interface TWBackdropProps {
-  overlayOpen: boolean
+  overlayOpen: boolean;
 }
 
 interface TWBoxProps extends BoxProps {
-  noValidate?: boolean // 追加プロパティを許容
+  noValidate?: boolean; // 追加プロパティを許容
 }
 
 interface EmailAuthProps {
-  inputNum: number
-  inputRefs: React.MutableRefObject<(HTMLInputElement | null)[]>
-  code: string[]
-  setCode: (value: React.SetStateAction<string[]>) => void
-  handleEntryEmail: () => Promise<void>
-  isDisabled: boolean
-  setIsDisabled: React.Dispatch<React.SetStateAction<boolean>>
+  inputNum: number;
+  inputRefs: React.MutableRefObject<(HTMLInputElement | null)[]>;
+  code: string[];
+  setCode: (value: React.SetStateAction<string[]>) => void;
+  handleEntryEmail: () => Promise<void>;
+  isDisabled: boolean;
+  setIsDisabled: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 interface EmailAuthToastProps {
-  successOverlayOpen: boolean
-  successOpen: boolean
-  successHandleClose: () => void
-  successMsg: string
-  overlayOpen: boolean
-  open: boolean
-  handleClose: () => void
-  msg: string
+  successOverlayOpen: boolean;
+  successOpen: boolean;
+  successHandleClose: () => void;
+  successMsg: string;
+  overlayOpen: boolean;
+  open: boolean;
+  handleClose: () => void;
+  msg: string;
+}
+
+interface WholesaleLineProps {
+  name: string;
 }
 
 interface BaseCallbaclToastProps {
-  overlayOpen: boolean
-  open: boolean
-  handleClose: () => void
-  msg: string
+  overlayOpen: boolean;
+  open: boolean;
+  handleClose: () => void;
+  msg: string;
 }
 
-type SignInToastProps = BaseCallbaclToastProps
-type SignUpToastProps = BaseCallbaclToastProps
+type SignInToastProps = BaseCallbaclToastProps;
+type SignUpToastProps = BaseCallbaclToastProps;
 
 interface EmailSendProps {
-  handleSubmit: UseFormHandleSubmit<EmailCheckProps, undefined>
-  control: Control<EmailCheckProps, unknown>
-  isValid: boolean
-  onSubmit: SubmitHandler<EmailCheckProps>
+  handleSubmit: UseFormHandleSubmit<EmailCheckProps, undefined>;
+  control: Control<EmailCheckProps, unknown>;
+  isValid: boolean;
+  onSubmit: SubmitHandler<EmailCheckProps>;
 }
 
 interface ExternalSignButtonProps {
-  label: string
-  icon?: React.ReactNode
-  onClick: () => void
+  label: string;
+  icon?: React.ReactNode;
+  onClick: () => void;
 }
 
 interface TWCircularProgressProps extends CircularProgressProps {
-  open: boolean
+  open: boolean;
 }
 
 interface TWExternalTextProps {
-  text: string
+  text: string;
 }
 export type {
   AnnualIncomeManagementKey,
   AnnualIncomeManagementData,
   AnnualIncomeManagementCreateData,
   AnnualIncomeManagementUpdateData,
+  RequestDepositCalculation,
   Validate,
   Column,
   ColumnNotEdit,
@@ -373,6 +401,7 @@ export type {
   FileSelectProps,
   CsvImportMainProps,
   TextFormProps,
+  DepositCalculationFormProps,
   PasswordFormProps,
   AuthFormProps,
   NewPasswordUpdateProps,
@@ -390,6 +419,7 @@ export type {
   RequestSigninProps,
   RequestExternalAuth,
   RequestTmpSignUpResProps,
+  WholesaleLineProps,
   RequestDataProps,
   RequestSignUpProps,
   RequestNewPasswordUpdateProps,
@@ -405,4 +435,4 @@ export type {
   ExternalSignButtonProps,
   TWExternalTextProps,
   PasswordUpdateFormProps,
-}
+};
